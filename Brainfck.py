95d8eb8ca838ccbca86867d0f64aba286487bd8c
hellscorpion <hellscorpionxx@gmail.com>
2017-10-26 17:48:51 +0200

    Initial commit

---
+++ Brainfck.py
@@ -0,0 +1,144 @@
+from sys import argv
+from sys import exit
+
+class Memory:
+    __maxValue = 127
+    __mem = []
+    __ptr = 0
+
+    def __init__(self):
+        self.__mem.append(0)
+
+    def go_right(self):
+        if self.__mem.__len__() <= self.__ptr+1:
+            self.__mem.append(0)
+        self.__ptr+=1
+
+    def go_left(self, iptr):
+        if(self.__ptr - 1 < 0):
+            r = "Index out of Bounds (Char at " + iptr.__str__() +"): Memory index cannot be negative!"
+            exit(r)
+        self.__ptr-=1
+
+    def decrease(self, iptr):
+        if(self.__mem[self.__ptr] - 1 < 0):
+            r = "Illegal State (Char at " + iptr.__str__() + "): Memorycellvalue cannot be negative"
+            exit(r)
+        self.__mem[self.__ptr]-=1
+
+    def increase(self, iptr):
+        if (self.__mem[self.__ptr] + 1 > self.__maxValue):
+            r = "Illegal State (Char at " + iptr.__str__() + "): Memorycellvalue cannot exceed 16Byte"
+            exit(r)
+        self.__mem[self.__ptr]+=1
+
+    def get_value(self):
+        return self.__mem[self.__ptr]
+
+    def set_value(self, value, iptr):
+        if value > self.__maxValue:
+            r = "Illegal State (Char at " + iptr.__str__() + "): Memorycellvalue cannot exceed 16Byte"
+            exit(r)
+        self.__mem[self.__ptr] = value
+
+
+
+
+
+
+
+
+# initializing code from arguments
+
+code = ""
+args = argv.copy()
+args[0] = ""
+for arg in args:
+    code+=arg
+
+# starting console output
+
+print("Executing code:")
+print(code)
+print("")
+print("-------------------------------------------\n\n")
+
+
+# counting loops
+
+loopBeginCount = 0
+loopEndCount = 0
+
+for i in range(0,code.__len__()):
+    if code[i] == "[":
+        loopBeginCount+=1
+    elif code[i] == "]":
+        loopEndCount+=1
+
+# checking consistency
+
+if loopBeginCount != loopEndCount:
+    exit("loops inconsistent")
+
+loopCount = loopBeginCount.__int__()
+
+del loopBeginCount
+del loopEndCount
+
+# determining loops
+
+loopBegin = []
+loopEnd   = []
+loopDepth = 0
+
+
+for i in range(0, code.__len__()):
+    if code[i] == "[":
+        loopDepth+=1
+        for j in range(i+1, code.__len__()):
+            if code[j] == "]":
+                loopDepth-=1
+                if loopDepth == 0:
+                    loopBegin.append(i)
+                    loopEnd.append(j)
+                    break
+            elif code[j] == "[":
+                loopDepth+=1
+
+
+
+# initializing instruction pointer and wokring-memory
+
+iptr = 0
+mem = Memory()
+
+# main loop
+
+while(iptr < code.__len__()):
+    if code[iptr] == '+':
+        mem.increase(iptr)
+    elif code[iptr] == '-':
+        mem.decrease(iptr)
+    elif code[iptr] == '<':
+        mem.go_left(iptr)
+    elif code[iptr] == '>':
+        mem.go_right()
+    elif code[iptr] == ']':
+        if mem.get_value() != 0:
+            for i in range(0, loopCount):
+                if loopEnd[i] == iptr:
+                    iptr = loopBegin[i]
+                    break
+    elif code[iptr] == ".":
+        print(chr(mem.get_value()), end='')
+    elif code[iptr] == ",":
+        tmp = input()
+        tmpint = ord(tmp[0])
+        mem.set_value(tmpint, iptr)
+
+
+    iptr+=1
+
+# end text
+
+print("\n\n-------------------------------------------")
